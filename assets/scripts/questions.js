47392334f11314b3b9b4de5e04f8283368063b8a
destro <irtizasafdar@hotmail.co.uk>
2024-03-20 01:03:40 +0000

    cleared previous placeholder from readme, created questions.js - populated with questions object - imported to app.js file - both js files added to index.html

---
+++ assets/scripts/questions.js
@@ -0,0 +1,58 @@
+const questions = [
+	{
+		prompt: `Commonly used data types DO NOT include: `,
+		options: [
+			"1.strings",
+			"2.booleans",
+			"3.alerts",
+			"4.numbers",
+		],
+		answer: "3.alerts",
+	},
+
+	{
+		prompt: `Arrays in JavaScript can be used to store _____.`,
+		options: [
+			"1.numbers and strings",
+			"2.other arrays",
+			"3.booleans",
+			"4.all of the above",
+		],
+		answer: "4.all of the above",
+	},
+
+	{
+		prompt: `A very useful tool used during development and debugging for printing content to the debugger is: `,
+		options: [
+			"1.JavaScript",
+			"2.terminal/bash",
+			"3.for loops",
+			"4.console.log",
+		],
+		answer: "4.console.log",
+	},
+
+	{
+		prompt: `String values must be enclosed within ____ when being assigned to variables.`,
+		options: [
+            "1.commas",
+            "2.curly brackets",
+            "3.quotes",
+            "4.parentheses"
+        ],
+		answer: "3.quotes",
+	},
+
+	{
+		prompt: `The condition in an if / else statement is enclosed within ____`,
+		options: [
+			"1.quotes",
+			"2.curly brackets",
+			"3.parentheses",
+			"4.square brackets",
+		],
+		answer: "3.parentheses",
+	},
+];
+
+export default questions 
